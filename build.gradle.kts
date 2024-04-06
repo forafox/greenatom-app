e77267298823aa33f09148baf044dd20d7b8b096
Андрей Карабанов <deskpa@yandex.ru>
2024-04-06 23:42:19 +0300

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+}
+
+group = "org.forafox"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("com.h2database:h2:2.2.224")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
